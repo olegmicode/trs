f2d0c29bcc6ab14430aaf89ae1ba9c8d864fec44
olegmicode <olegmijovic.io@gmail.com>
2021-04-06 13:26:16 -0500

    disable rets sources

--- trs-gatsby/gatsby-config.js
+++ trs-gatsby/gatsby-config.js
@@ -50,8 +50,8 @@ module.exports = {
       },
     },
     "gatsby-plugin-theme-ui",
-    require.resolve(`./digett-source-idx`),
-    require.resolve(`./digett-source-kerrville`),
+    // require.resolve(`./digett-source-idx`),
+    // require.resolve(`./digett-source-kerrville`),
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
