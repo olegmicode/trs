e84e67449fcc14b463ee39e59811977c4584d427
Alexander Fountain <fountain@AlexandersMBP2.PK5001Z>
2021-05-14 17:08:45 -0500

    sort fix

--- trs-gatsby/src/pages/favorites.js
+++ trs-gatsby/src/pages/favorites.js
@@ -8,7 +8,7 @@ class Favorites extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      favorites: null,
+      favorites: [{}],
     }
   }
   componentWillMount() {
