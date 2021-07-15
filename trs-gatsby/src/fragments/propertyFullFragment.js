959427ac07ac0cf245e5db0b545d950c2e3f0815
Alexander Fountain <fountain@AlexandersMBP2.PK5001Z>
2021-07-15 15:49:41 -0500

    inference in fragment

--- trs-gatsby/src/fragments/propertyFullFragment.js
+++ trs-gatsby/src/fragments/propertyFullFragment.js
@@ -2,8 +2,8 @@ import { graphql } from "gatsby"
 
 export const propertyFullFragment = graphql`
   fragment propertyFullFragment on property {
-    mlsid: String!
-    id: String!
+    mlsid: String
+    id: String
     acreage: Int
     county: String
     propertyDescription: String
