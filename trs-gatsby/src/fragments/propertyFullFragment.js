146cfcba37c4e348e17dbc3981cefabd0e078078
Alexander Fountain <fountain@AlexandersMBP2.PK5001Z>
2021-07-15 15:47:31 -0500

    inference in fragment

--- trs-gatsby/src/fragments/propertyFullFragment.js
+++ trs-gatsby/src/fragments/propertyFullFragment.js
@@ -2,12 +2,12 @@ import { graphql } from "gatsby"
 
 export const propertyFullFragment = graphql`
   fragment propertyFullFragment on property {
-    mlsid String!
-    id String!
-    acreage Int
-    county String
-    propertyDescription String
-    price Int
+    mlsid: String!
+    id: String!
+    acreage: Int
+    county: String
+    propertyDescription: String
+    price: Int
     childrenFile {
       childImageSharp {
         gatsbyImageData
