891fd76b9e508daeb32589a2af2d756d3c4256b2
alexanderfountain <alexanderfountain@gmail.com>
2022-05-20 10:30:38 -0500

    contact initial styles

--- trs-gatsby/src/components/contactForm.js
+++ trs-gatsby/src/components/contactForm.js
@@ -59,8 +59,18 @@ const MyForm = () => {
           sx={{
             display: "flex",
             flexDirection: "column",
-            alignItems: "flex-end",
-            justifyContent: "flex-end",
+            alignItems: "flex-start",
+            select: {
+              border: "1px solid #484242",
+              color: "#484242",
+              backgroundColor: "transparent",
+              padding: "8px 20px",
+              fontSize: "1rem",
+              width: "100%",
+              borderRadius: "0px !important",
+              boxSizing: "border-box",
+              marginTop: "20px",
+            },
             input: {
               border: "1px solid #484242",
               color: "#484242",
@@ -70,9 +80,7 @@ const MyForm = () => {
               width: "100%",
               borderRadius: "0px !important",
               boxSizing: "border-box",
-              "&:nth-child(2)": {
-                margin: "15px 0px",
-              },
+              marginTop: "20px",
               "&::placeholder": {
                 color: "#484242",
               },
@@ -88,26 +96,37 @@ const MyForm = () => {
               padding: "8px 30px",
               fontSize: "1rem",
               cursor: "pointer",
+              display: "inline-block",
+              marginTop: "20px",
+              marginBottom: "20px",
+            },
+            ".check-container": {
+              display: "flex",
+              width: "100%",
+              alignItems: "center",
+              marginTop: "20px",
+              input: {
+                width: "20px",
+                height: "20px",
+                margin: "0px 20px 0px 0px",
+              },
+              label: {
+                width: "calc(100% - 40px)",
+              },
             },
           }}
           onSubmit={handleOnSubmit}
         >
-          <input placeholder="Name" type="text" required name="name" />
-          <label>Email*</label>
-          <input type="email" required name="email" />
-          <label>Phone</label>
-          <input type="phone" name="phone" />
-          <label>Best Time to Call*</label>
+          <input placeholder="Name*" type="text" required name="name" />
+          <input placeholder="Email*" type="email" required name="email" />
+          <input placeholder="Phone" type="phone" name="phone" />
           <select name="time" required>
+            <option value="">Best Time to Call*</option>
             <option value="anytime">Anytime</option>
             <option value="am">AM</option>
             <option value="pm">PM</option>
           </select>
-          <div
-            sx={{
-              display: "flex",
-            }}
-          >
+          <div className="check-container">
             <input type="checkbox" name="subscribe_to_newsletter" value="1" />
             <label>
               Yes, please sign me up to receive the Texas Ranches For Sale
@@ -115,45 +134,38 @@ const MyForm = () => {
             </label>
           </div>
 
-          <label>Please Select All That Apply Below:</label>
           <div
             sx={{
-              display: "flex",
+              width: "100%",
+              fontWeight: "bold",
+              fontSize: "1.2rem",
+              marginTop: "20px",
             }}
           >
+            Please Select All That Apply Below:
+          </div>
+          <div className="check-container">
             <input type="checkbox" name="agent_contact" value="1" />
             <label>
               I would like to know more about this property, please have an
               agent contact me directly
             </label>
           </div>
-          <div
-            sx={{
-              display: "flex",
-            }}
-          >
+          <div className="check-container">
             <input type="checkbox" name="appointment" value="1" />
             <label>
               I would like to make an appointment to view this property
             </label>
           </div>
 
-          <div
-            sx={{
-              display: "flex",
-            }}
-          >
+          <div className="check-container">
             <input type="checkbox" name="evaluation" value="1" />
             <label>
               I am interested in a current market evaluation of my property
             </label>
           </div>
 
-          <div
-            sx={{
-              display: "flex",
-            }}
-          >
+          <div className="check-container">
             <input type="checkbox" name="autoalerts" value="1" />
             <label>
               I would like to receive automatic email alerts when similar
@@ -161,16 +173,36 @@ const MyForm = () => {
             </label>
           </div>
 
-          <div
-            sx={{
-              display: "flex",
-            }}
-          >
+          <div className="check-container">
             <input type="checkbox" name="haveagent" value="1" />
             <label>I am currently working with a real estate agent</label>
           </div>
-          <label>Comments</label>
-          <textarea required name="comments" />
+          <div
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div
+              sx={{
+                width: "100%",
+                fontWeight: "bold",
+                fontSize: "1.2rem",
+                marginTop: "20px",
+                marginBottom: "20px",
+              }}
+            >
+              Comments
+            </div>
+            <textarea
+              sx={{
+                height: "150px",
+              }}
+              required
+              name="comments"
+            />
+          </div>
           <input type="hidden" name="team" value={serverState.team} />
           <button type="submit">Send</button>
         </form>
