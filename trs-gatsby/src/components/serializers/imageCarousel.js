3219d89107257f9a2fdb41503c36f38365431974
hectorstudio <5674062+hectorstudio@users.noreply.github.com>
2022-06-22 15:17:48 -0400

    fix: fixed feedbacks

--- trs-gatsby/src/components/serializers/imageCarousel.js
+++ trs-gatsby/src/components/serializers/imageCarousel.js
@@ -5,11 +5,10 @@ import { convertToBgImage } from "gbimage-bridge"
 import BackgroundImage from "gatsby-background-image"
 
 import Container from "../container"
-import useIsMobile from "../../hooks/useIsMobile";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 
-const DesktopImage = ({ image }) => {
+const Image = ({ image }) => {
   const sanityConfig = { projectId: "5b1rgyjn", dataset: "production" }
   const imageAssetId = image.asset.id
 
@@ -33,33 +32,7 @@ const DesktopImage = ({ image }) => {
   )
 };
 
-const MobileImage = ({ image }) => {
-  const sanityConfig = { projectId: "5b1rgyjn", dataset: "production" }
-  const imageAssetId = image.asset.id
-
-  const imageData = getGatsbyImageData(
-    imageAssetId,
-    { maxWidth: 675 },
-    sanityConfig
-  )
-  const bgImage = convertToBgImage(imageData)
-  return (
-    <BackgroundImage
-      Tag="div"
-      // Spread bgImage into BackgroundImage:
-      {...bgImage}
-      preserveStackingContext
-      sx={{
-        padding: "100px 60px",
-      }}
-    >
-    </BackgroundImage>
-  )
-};
-
 const ImageCarousel = ({ node }) => {
-  const isMobile = useIsMobile();
-
   if (!node.body) {
     return <div />
   }
@@ -75,10 +48,14 @@ const ImageCarousel = ({ node }) => {
           infiniteLoop
           showIndicators={false}
           showArrows={false}
+          showStatus={false}
+          interval={5000}
+          animationHandler="fade"
+          swipeable={false}
         >
           {node.body.map(el => (
-            <div>
-              {isMobile ? <MobileImage image={el.mobileVersion} /> : <DesktopImage image={el.desktopVersion} />}
+            <div className="carousel-image">
+              <Image image={el.desktopVersion} />
             </div>
           ))}
         </Carousel>
